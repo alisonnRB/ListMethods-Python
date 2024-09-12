caca20fc5722ca8bd183c480a3e5f2f0edab6fd2
Discentes IFRS Rolante 2024 <alisonrbjs@gmail.com>
2024-09-11 22:00:23 -0300

    cretaed

---
+++ BuscaBinaria/main.py
@@ -0,0 +1,49 @@
+from models import Utilities
+
+class Main:
+    def __init__(self) -> None:
+        self.clean = Utilities.Utilities() 
+        print("Bem-vindo ao Search do Álison.")
+        with open('./palavras/palavras.txt', 'r') as arquivo:
+            self.ordenadas = [linha.rstrip() for linha in arquivo]
+
+        self.clean.clean(2)  
+        self.inputValue()
+
+    def inputValue(self):
+        print("Qual nome deseja procurar nas listas?")
+        value = input("")
+        index = self.search(value)
+
+        if(isinstance(index, int)):
+            print(f"A palavra {value} está na posição {index + 1}")
+        else:
+            print(f"a palavra {value} não está na lista")
+        
+        self.clean.clean(2)  
+        self.inputValue()
+
+    def search(self, value):
+        continuar = True
+        max = len(self.ordenadas) - 1
+        min = 0
+
+        while continuar:
+            if(min > max):
+                continuar = False
+            else:
+                index = (max + min ) // 2
+                middle_word = self.ordenadas[index]
+
+                if(middle_word == value):
+                    return index
+                elif(middle_word < value):
+                    min = index + 1
+                else:
+                   max = index - 1
+
+        return "ERRO"
+        
+
+if __name__ == "__main__":
+    Main()
